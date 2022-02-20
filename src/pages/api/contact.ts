82863dcacb7082b50ea412691f154dcdb6330fc3
Rascal_Two <therealrascaltwo@gmail.com>
2022-02-19 19:48:33 -0500

    Add referer and origin headers to contact API

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -8,11 +8,12 @@ const transporter = nodemailer.createTransport(TRANSPORT);
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { referer, origin } = req.headers;
   const { name='', email='', phone='', website='', message='' } = req.body ?? {};
   if (!name || !message) return res.status(400).end('Name and Message are both required fields!');
   await transporter.sendMail({
     ...SENDMAIL,
-    text: `Contact Message received at ${Date.now()}: ${JSON.stringify({ name, email, phone, website, message}, null, '  ')}`
+    text: `Contact Message received at ${Date.now()}: ${JSON.stringify({ name, email, phone, website, message, referer, origin }, null, '  ')}`
   })
   return res.status(200).send('Thank you for the message!\nI\'ll get back to you within one business day!');
 }
