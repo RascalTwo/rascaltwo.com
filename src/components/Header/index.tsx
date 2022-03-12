b1b3ab1447eb73419fb1b2f6f37d63a78cc8f836
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-11 22:58:09 -0500

    Add lower quality header images

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,26 @@
-import { useCallback } from 'react'
+import { CSSProperties, useCallback, useEffect, useState } from 'react'
 import Typewriter from '../Typewriter'
 import styles from './Header.module.css'
 
 export default function Header({ name }: { name: string }){
+	const [bgUrl, setBGUrl] = useState('header-low.webp');
+
+	useEffect(() => {
+		if (bgUrl === 'header-high.png') return;
+		const timeout = setTimeout(
+			() => setBGUrl(`header-${bgUrl === 'header-low.webp' ? 'medium' : 'high'}.png`),
+			bgUrl === 'header-low.webp' ? 5000 : 10000
+		);
+		return () => clearTimeout(timeout);
+	}, [bgUrl]);
+
 	return (
-		<header className={styles.header}>
+		<header
+			className={styles.header}
+			style={{
+				'--bg-url': `url(/${bgUrl})`
+			} as CSSProperties}
+		>
 			<span>
 				<h1>{name}</h1>
 				<Typewriter
