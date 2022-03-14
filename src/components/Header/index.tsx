0f25f33485f0f91e359c46bd5f1206968a08349b
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-14 14:02:27 -0400

    Add header animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,67 @@
-import { CSSProperties, useCallback, useEffect, useState } from 'react'
-import Typewriter from '../Typewriter'
-import styles from './Header.module.css'
+import { CSSProperties, useCallback, useEffect, useMemo, useState } from 'react';
+import Typewriter from '../Typewriter';
+import styles from './Header.module.css';
 
-export default function Header({ name }: { name: string }){
-	const [bgUrl, setBGUrl] = useState('header-low.webp');
+function Name({ name }: { name: string }) {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => setLoaded(true), []);
 
-	useEffect(() => {
-		if (bgUrl === 'header-high.png') return;
-		const timeout = setTimeout(
-			() => setBGUrl(`header-${bgUrl === 'header-low.webp' ? 'medium' : 'high'}.png`),
-			bgUrl === 'header-low.webp' ? 5000 : 10000
-		);
-		return () => clearTimeout(timeout);
-	}, [bgUrl]);
+  const letters = useMemo(() => name.split(''), [name]);
+  const rotOffset = useMemo(() => 0.5 - letters.length / 2, [letters]);
 
-	return (
-		<header
-			className={styles.header}
-			style={{
-				'--bg-url': `url(/${bgUrl})`
-			} as CSSProperties}
-		>
-			<span>
-				<h1>{name}</h1>
-				<Typewriter
-					phrases={[
-						"Software Engineer",
-						"Fullstack Developer",
-						"Polyglot"
-					]}
-					typingRate={250}
-					pauseMS={[0, 10000]}
-					initialIndex={8}
-					render={useCallback(text => <span>{text}</span>, [])}
-				/>
-			</span>
-		</header>
-	)
+  return (
+    <h1 className={styles.name} data-loaded={loaded} aria-label={name}>
+      {letters.map((l, i) => (
+        <div
+          key={i}
+          style={
+            {
+              '--degrees': (i + rotOffset) * 10 + 'deg',
+              '--delay': 2750 + i * 100 + 'ms',
+            } as CSSProperties
+          }
+        >
+          {l}
+        </div>
+      ))}
+    </h1>
+  );
+}
+
+export default function Header({ name }: { name: string }) {
+  const [bgUrl, setBGUrl] = useState('header-low.webp');
+
+  useEffect(() => {
+    if (bgUrl === 'header-high.png') return;
+    const timeout = setTimeout(
+      () => setBGUrl(`header-${bgUrl === 'header-low.webp' ? 'medium' : 'high'}.png`),
+      bgUrl === 'header-low.webp' ? 10000 : 20000,
+    );
+    return () => clearTimeout(timeout);
+  }, [bgUrl]);
+
+  return (
+    <header
+      className={styles.header}
+      style={
+        {
+          '--bg-url': `url(/${bgUrl})`,
+        } as CSSProperties
+      }
+    >
+      <Name name={name} />
+      <Typewriter
+        phrases={['Software Engineer', 'Fullstack Developer', 'Polyglot']}
+        typingRate={250}
+        pauseMS={[0, 10000]}
+        initialIndex={8}
+        render={useCallback(
+          text => (
+            <span>{text}</span>
+          ),
+          [],
+        )}
+      />
+    </header>
+  );
 }
