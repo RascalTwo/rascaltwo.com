ac51e8d976e4b34ac7638425d790f559b6301f29
Rascal_Two <therealrascaltwo@gmail.com>
2022-03-15 10:08:20 -0400

    fixup inclusive

--- src/hooks.ts
+++ src/hooks.ts
@@ -5,14 +5,18 @@ export interface R2Set<T> {
 	setSet: Dispatch<SetStateAction<Set<T>>>
 	add: (value: T | ((prevState: Set<T>) => T)) => void
 	delete: (value: T | ((prevState: Set<T>) => T)) => void
+	lastAdded: T | null
 }
 
 export const useSet = <T extends unknown>(initialState: Set<T> | (() => Set<T>) = () => new Set()): R2Set<T> => {
 	const [set, setSet] = useState<Set<T>>(initialState);
+	const [lastAdded, setLastAdded] = useState<T|null>(null);
 
 	const addFunc = useCallback((value: T | ((prevState: Set<T>) => T)) => {
 		const newSet = new Set(set);
-		newSet.add(value instanceof Function ? value(set) : value)
+		const newValue = value instanceof Function ? value(set) : value
+		newSet.add(newValue);
+		setLastAdded(newValue);
 		setSet(newSet);
 	}, [set]);
 
@@ -22,6 +26,6 @@ export const useSet = <T extends unknown>(initialState: Set<T> | (() => Set<T>) 
 		setSet(newSet);
 	}, [set]);
 
-	return { set, setSet, add: addFunc, delete: deleteFunc }
+	return { set, setSet, add: addFunc, delete: deleteFunc, lastAdded }
 }
 
